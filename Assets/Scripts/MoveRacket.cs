bd54938f439cd0bd6cdf8366ec27ecfa2e2b0791
dcacciattolo <dcac2094@gmail.com>
2017-04-24 16:27:56 +0200

    Setting the controls of the rackets individually.
    Still not working properly.

--- Assets/Scripts/MoveRacket.cs
+++ Assets/Scripts/MoveRacket.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class MoveRacket : MonoBehaviour {
 
     public float speed = 30;
+    public string axis = "Vertical";
 
     private void FixedUpdate()
     {
         float v = Input.GetAxisRaw("Vertical");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
     }
 }
